d588e1234b9929219a6afcfcec5842d24ac7e2bc
Fernando Silva <fernando457829@gmail.com>
2021-10-21 22:07:16 -0300

    add basic image creation

--- minimalistic-icons/main.py
+++ minimalistic-icons/main.py
@@ -3,11 +3,23 @@
 """
 
 import click
+from PIL import Image, ImageColor
 
 @click.command()
-def main():
+@click.argument('color', type=click.STRING, required=True)
+@click.option('-s', '--size', type=click.INT, default=512, show_default=True,
+              help='The size of image.')
+@click.option('-f', '--file', type=click.STRING, default='image.png', show_default=True,
+              help='Output file')
+def main(color, size, file):
     """Create a minimalistic icon"""
-    print("Hello World")
+
+    image_color = ImageColor.getrgb(color)
+    image = Image.new('RGB', (size, size), image_color)
+
+    image.save(file)
+
+    click.echo('Image created')
 
 if __name__ == '__main__':
-    main()
+    main() # pylint: disable=no-value-for-parameter
